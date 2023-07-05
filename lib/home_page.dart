aa64b97cc7ae85a67f642053f53a6ad81dc42b6b
chucho5280 <chucho5280@hotmail.com>
2023-07-05 01:20:24 -0600

    Ui Responsive Correcto

---
+++ lib/home_page.dart
@@ -0,0 +1,93 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      
+      child: Scaffold(
+        body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _header(context),
+              _inputField(context),
+              Column(
+              children: [
+                Image.asset('assets/img/foto.jpg',
+                height: 300.0,
+                width: 300.0,
+          ),],
+          ),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+
+  _header(context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Text(
+          "Inicio Sesion",
+          style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
+        ),
+        Text("Ingresa tus datos para sacrificarte"),
+      ],
+    );
+  }
+
+  _inputField(context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        TextField(
+          decoration: InputDecoration(
+              hintText: "Usuario",
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(18),
+                  borderSide: BorderSide.none),
+              fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
+              filled: true,
+              prefixIcon: Icon(Icons.person)),
+        ),
+        SizedBox(height: 10),
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Contraseña",
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(18),
+                borderSide: BorderSide.none),
+            fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
+            filled: true,
+            prefixIcon: Icon(Icons.person),
+          ),
+          obscureText: true,
+        ),
+        SizedBox(height: 10),
+        ElevatedButton(
+          onPressed: () {},
+          child: Text(
+            "Login",
+            style: TextStyle(fontSize: 20),
+          ),
+          style: ElevatedButton.styleFrom(
+            shape: StadiumBorder(),
+            padding: EdgeInsets.symmetric(vertical: 16),
+          ),
+        )
+      ],
+    );
+  }
+
+  
+
+}
